4957fd07ff923c9b240bd4ccb007709af7447378
Mihir Maroju <91926675+mikidoodle@users.noreply.github.com>
2023-05-12 13:09:06 +0530

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,74 @@
 import Head from "next/head";
 import Script from "next/script";
+import { useEffect, useState } from "react";
+import Modal from "react-modal";
 import "../styles/globals.scss";
+
 function MyApp({ Component, pageProps }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isConnected, setIsConnected] = useState(false);
+  const customStyles = {
+    overlay: {
+      backgroundColor: "rgba(0, 0, 0, 0.6)",
+    },
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      borderRadius: "15px",
+    },
+  };
+  const customStyles2 = {
+    overlay: {
+      backgroundColor: "rgba(0, 0, 0, 0.6)",
+    },
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      borderRadius: "15px",
+      padding: "20px 150px",
+    },
+  };
+  useEffect(() => {
+    window.addEventListener("online", () => {
+      setIsOpen(false);
+      setIsConnected(true);
+    });
+    window.addEventListener("offline", () => {
+      setIsOpen(true);
+      setIsConnected(false);
+    });
+  }, []);
   return (
-    <>
+    <div id="appElement">
+      <Modal isOpen={isOpen} style={customStyles}>
+        <h1 style={{ fontSize: "48px", textAlign: "center" }}>:/</h1>
+        <h3>Your device isn't connected to the Internet.</h3>
+        <p style={{ textAlign: "center" }}>
+          Check your connection and try again.
+        </p>
+      </Modal>
+      <Modal
+        isOpen={isConnected}
+        onRequestClose={() => setIsConnected(false)}
+        style={customStyles2}
+      >
+        <h1 style={{ fontSize: "48px", textAlign: "center" }}>:)</h1>
+        <h3>Back online!</h3>
+        <button onClick={() => setIsConnected(false)}>Dismiss</button>
+      </Modal>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-        crossOrigin="anonymous"
-      />
-      <Component {...pageProps} />
-    </>
+     {isOpen ? null : <Component {...pageProps} />}
+    </div>
   );
 }
 export default MyApp;
